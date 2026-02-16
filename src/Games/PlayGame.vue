d2f3974ce3916811c6fd0feadc36083a8bcd7e65
Adilson A. Mattos <adilson.day@gmail.com>
2026-02-16 13:19:07 -0300

    controle rapido para para adicionar/remover jogos

--- src/Games/PlayGame.vue
+++ src/Games/PlayGame.vue
@@ -2,21 +2,18 @@
 
 import { onMounted, ref, provide } from 'vue';
 
-
 import GamesService from '@/services/GamesService';
 import PageLayout from '@/Components/PageLayout.vue';
 import CardComponent from '@/Components/CardComponent.vue';
 
-
+const refreshToken = ref(0);
 const listOfGames = ref([]);
 const listOfPaused = ref([]);
 const columnsLayout = ref("");
-const refreshToken = ref(0);
 
 const getPlayingGame = async () => {
     try {
         const response = await GamesService.getByStatus({statusCompra: 'Adquirido', status: 'Jogando'});
-        
 
         if (!response) {
             alert("Não encontrado");
@@ -61,11 +58,6 @@ const addToPause = async (game) => {
 
 const updateGame = async (game) => {
     const response = await GamesService.update(game._id, game);
-
-    if (response) {
-        alert("Sucesso", response);
-    }
-
     await callPageInformation(); 
 }
 
@@ -88,11 +80,26 @@ provide("columnsLayout", columnsLayout);
         </template>
     </div>
 
-    <div class="mt-4 p-4">
-       
-        <h3 class="text-xl font-medium mb-4">Pausados</h3>
+    <div id="foldable" v-if="listOfGames && listOfPaused">
+        <div class="mt-4 p-4">
+            <h3 class="text-xl font-medium mb-4">Jogando</h3>
+            <template v-for="game in listOfGames">
+                
+                <div class="border-gray-700 border rounded-md hover:bg-gray-800 p-2 flex justify-between">
+                    <span>{{ game.titulo }}</span>
+                    <div>
+                        
+                        <button class="button button-color" @click="addToPause(game)">Pausar</button>
+                        <button class="button button-color" @click="addToCancel(game)">Cancelar</button>
+                    </div>
+                </div>
+            </template>
+        </div>
+        <div class="mt-4 p-4">
+            
+            <h3 class="text-xl font-medium mb-4">Pausados</h3>
             <template v-for="game in listOfPaused">
-
+                
                 <div class="border-gray-700 border rounded-md hover:bg-gray-800 p-2 flex justify-between">
                     <span>{{  game.titulo }}</span>
                     <div>
@@ -102,6 +109,7 @@ provide("columnsLayout", columnsLayout);
                     </div>
                 </div>
             </template>
+        </div>
     </div>
 
 </template>
